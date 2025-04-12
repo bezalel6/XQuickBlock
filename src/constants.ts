71d7b1bc1c9260fcc8c6ceb590efecb68e6014d5
bezalel6 <bezalel3250@gmail.com>
2025-04-12 18:43:36 +0300

    Include premium upsale tab so it can  be hidden

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export const confirmDialogConfirmSelector = `[data-testid="confirmationSheetConf
 export const userMenuSelector = '[aria-label="More"]'
 export const upsaleTwitterPathname = 'i/verified-get-verified'
 export const buyIntoUpsaleHref = "/i/premium_sign_up"
-export const upsaleSelectors = ['[data-testid="verified_profile_upsell"]', `aside:has( a[href="${buyIntoUpsaleHref}"])`]
+export const upsaleSelectors = ['[data-testid="verified_profile_upsell"]', `aside:has( a[href="${buyIntoUpsaleHref}"])`, `a[href="${buyIntoUpsaleHref}"]`]
 export const upsaleDialogSelector = '[data-testid="sheetDialog"]'
